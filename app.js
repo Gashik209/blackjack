4e4dda62cc5512a0c27c8be74b1c60730f29e3ab
Gashik209 <Gashik209@gmail.com>
2016-10-25 11:46:55 +0300

    add remember_me duration, fix X2 player status

--- app.js
+++ app.js
@@ -18,16 +18,9 @@ var config=require(__dirname+"/config");
 var cookieParser=require("cookie-parser");
 var device = require('express-device');
 
-
-var server=require(__dirname+"/lib/server.js").server(express,http,io,socketcookieParser,bodyParser,favicon,logger,session,passport,LocalStrategy,RememberMeStrategy,config,cookieParser,device);
 var dbnedb=require(__dirname+"/lib/dbnedb.js").dbnedb(Datastore,__dirname+"/"+config.get("db:dbusers"),__dirname+"/"+config.get("db:dbusersStat"),__dirname+"/"+config.get("db:dbsessions"));
+var server=require(__dirname+"/lib/server.js").server(express,http,io,socketcookieParser,bodyParser,favicon,logger,session,passport,LocalStrategy,RememberMeStrategy,config,cookieParser,device);
 
 exports.fs=fs;
 exports.ejs=ejs;
 exports.bcrypt=bcrypt;
-
-
-
-var game=require(__dirname+"/lib/game.js");
-	game.initTable(5,50,"system");
-	game.initTable(10,100,"system");
